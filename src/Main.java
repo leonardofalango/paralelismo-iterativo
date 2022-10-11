7a79368eea3dca8d6a91cfb020611a6a0b0a69b4
Leonardo Scanavacca Falango <leonardo.falango@pucpr.edu.br>
2022-10-10 22:13:05 -0300

    feat: get_row and get_column

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         WriteFile.WriteFilePath("../MatrixA.txt", "notextallowed");
         double matrixA[][] = WriteFile.ReadFile("MatrixA.txt");
-        for (int i =0;i< matrixA.length;i++) for (int j=0;j< matrixA[i].length;j++) System.out.println(matrixA[i][j]);
+        Matrix matrix = new Matrix(matrixA);
+        //matrix.printMatrix();
 
+        //double coluna0[] = matrix.get_column(0);
+        //double linha0[] = matrix.get_row(0);
+
+        //for (int i=0;i<coluna0.length; i++) System.out.println(coluna0[i]);
     }
 }
